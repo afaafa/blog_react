fe7612d2da23e2d39cb4b06632cc4885375537ec
afaafa <afhazachra@gmail.com>
2021-01-30 17:23:28 +0700

    create crud react

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/article', ArticleController::class);
+Route::resource('/articles', ArticleController::class);
