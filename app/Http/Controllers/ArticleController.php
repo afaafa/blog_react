17e72c1811efadab758c2b2689a6bcff3bbe8af8
afaafa <afhazachra@gmail.com>
2021-01-30 10:48:27 +0700

    create migation, model, controller, & install ui react

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::all();
+ 
+        return $articles->toJson();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+          ]);
+   
+          $project = Article::create([
+            'title' => $validatedData['title'],
+            'content' => $validatedData['content'],
+          ]);
+   
+          $msg = [
+              'success' => true,
+              'message' => 'Article created successfully!'
+          ];
+   
+          return response()->json($msg);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $article = Article::find($id);
+ 
+        return $article->toJson();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $article = Article::find($id);
+ 
+        return $article->toJson();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+          ]);
+   
+          $article = Article::find($id);
+          $article->title = $validatedData['title'];
+          $article->content = $validatedData['content'];
+          $article->save();
+   
+          $msg = [
+              'success' => true,
+              'message' => 'Article updated successfully'
+          ];
+   
+          return response()->json($msg);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $article = Article::find($id);
+        if(!empty($article)){
+            $article->delete();
+            $msg = [
+                'success' => true,
+                'message' => 'Article deleted successfully!'
+            ];
+            return response()->json($msg);
+        } else {
+            $msg = [
+                'success' => false,
+                'message' => 'Article deleted failed!'
+            ];
+            return response()->json($msg);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/article', ArticleController::class);
